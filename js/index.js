f8d337107f44cef14ae8cec920879e99c36ca8c3
Guyw1046 <296563441@qq.com>
2016-06-01 18:16:18 +0800

    first push

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('body').on('click', 'nav div', function(){clickNav(this);});
+})
+
+function clickNav(that){
+    if($(that).hasClass("active")) return false;
+    $("nav div").removeClass("active");
+    $(that).addClass("active");
+    $("[data-toggle='tab']").hide();
+    $("[data-tab='"+that.id+"']").show();
+}
