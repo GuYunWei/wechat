a3c4b98b7376118b61b8382a7aa4c9f3b38c08cf
Guyw1046 <296563441@qq.com>
2016-06-05 15:47:13 +0800

    modify video play

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 $(function() {
+		getThumbnail();
     $('body').on('click', 'nav div', function(){clickNav(this);});
+    // $('body').on('tap', 'header', intialize);
+    $('body').on('click', 'header', playVideo);
 })
 
 function clickNav(that){
@@ -9,3 +12,50 @@ function clickNav(that){
     $("[data-toggle='tab']").hide();
     $("[data-tab='"+that.id+"']").show();
 }
+
+function getThumbnail () {
+	$.ajax({
+        url: 'https://pcs.baidu.com/rest/2.0/pcs/device',
+        type: "get",
+        data: {
+            method: "thumbnail",
+            shareid: "e2e63b40b7355e9e8192b02588375710",
+            uk: "878364372",
+            latest: '1'
+        },
+        dataType: "jsonp",
+        success: function (data) {
+        	console.log(data.list[0].url);
+        	if(data.list.length > 0){
+        		$("header>img").attr("src", data.list[0].url);
+        	}else{
+        		$("header>img").attr("src", "images/default-thumbnail.jpg");
+        	}
+        },
+        error: function (XMLHttpRequest, textStatus, errThrown) {}
+    })
+}
+
+function playVideo () {
+	$.ajax({
+        url: 'https://pcs.baidu.com/rest/2.0/pcs/device',
+        type: "get",
+        data: {
+            method: "liveplay",
+            shareid: "e2e63b40b7355e9e8192b02588375710",
+            uk: "878364372",
+            type: 'hls'
+        },
+        dataType: "jsonp",
+        //jsonpCallback: 'jsonpCallback',
+        success: function (data) {
+        	var div = data['div'];
+            $(".mask").show().append(data['div']);
+        },
+        error: function (XMLHttpRequest, textStatus, errThrown) {}
+    })
+}
+
+// function playVideo () {
+	
+// }
