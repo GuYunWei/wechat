65e2036552cfd4e9f70d22714782564514bf993d
Guyw1046 <296563441@qq.com>
2016-06-05 17:58:56 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ function playVideo () {
         	// myVideo.play();
         	var div = data['div'] + "<script>init_player('100%', 'auto')</script>";
           $(".mask").css({"width":"100%", "height":"100%", "display":"table"}).append(div);
-          $(".mask video").addClass("myVideo");
+          $(".mask video").addClass("myVideo").get(0).play();
         },
         error: function (XMLHttpRequest, textStatus, errThrown) {}
     })
