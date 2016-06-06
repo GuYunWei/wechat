f6f7295f74ff84dfa9946e74b3acab58cebe5af1
Guyw1046 <296563441@qq.com>
2016-06-06 16:15:37 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function() {
-		// document.addEventListener("WeixinJSBridgeReady", function () { 
-  //       document.getElementById('video').play(); 
-  //   }, false); 
+		document.addEventListener("WeixinJSBridgeReady", function () { 
+        document.getElementById('video').play(); 
+    }, false); 
   	getThumbnail();
 		intialize();
 		
@@ -24,8 +24,8 @@ function getThumbnail () {
         type: "get",
         data: {
             method: "thumbnail",
-            shareid: "42f904d242fd6e358a490f97a2013f23",
-            uk: "2050712015",
+            shareid: "e2e63b40b7355e9e8192b02588375710",
+            uk: "878364372",
             latest: '1'
         },
         dataType: "jsonp",
@@ -61,36 +61,15 @@ function intialize () {
 function tapVideo() {
 	if (browser.versions.ios || browser.versions.iPhone || browser.versions.iPad) {
 		$("#video").css({"width": "100%","height": "auto"}).parents(".wrap").css({"width":"100%","height":"100%"});
-	} else if (browser.versions.android) {
+	} else{
 		$("#video").css({"width": "100%","height": "auto"}).parents(".wrap").addClass("mask");
 	}
 }
 
 function clickVideo () {
 	$('video').get(0).play();
-	// $("video").parent().addClass("mask");
-    // $.ajax({
-    //     url: 'https://pcs.baidu.com/rest/2.0/pcs/device',
-    //     type: "get",
-    //     data: {
-    //         method: "liveplay",
-    //         shareid: "42f904d242fd6e358a490f97a2013f23",
-    //         uk: "2050712015",
-    //         type: 'hls'
-    //     },
-    //     dataType: "jsonp",
-    //     success: function (data) {
-    //     	console.log(data);
-    //     	// $(".mask").css({"width":"100%", "height":"100%"});
-    //     	// $("#video").addClass("myVideo").attr("src", data["src"]).get(0).play();
-    //     	// myVideo.play();
-    //     	var div = data['div'] + "<script>init_player('100%', 'auto')</script>";
-    //       $(".mask").css({"width":"100%", "height":"100%", "display":"table"}).append(div);
-    //       $(".mask video").addClass("myVideo").get(0).play();
-    //     },
-    //     error: function (XMLHttpRequest, textStatus, errThrown) {}
-    // })
 }
+
 var browser = {
 		versions: function() {
 			var u = navigator.userAgent,
